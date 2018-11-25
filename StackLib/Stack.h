9b26e11e032e9a4a2a9c19aba867407cbecbf1f7
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-11-25 03:50:12 +0300

    trying to make it work

---
+++ StackLib/Stack.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <iostream>
+
+template <class T>
+class TStack
+{
+protected:
+  int Size;
+  int Top;
+  T* Mas;
+public:
+  TStack(int n = 0);
+  TStack(TStack<T> &S);
+  void Put(T A);
+  T Get();
+  bool IsFull();
+  bool IsEmpty();
+};
+//----------------------------------------------------------------------
+template <class T>
+TStack <T> ::TStack(int n)
+{
+  if (n < 0) throw 1;
+  else
+	if (n == 0)
+	{
+	  Size = Top = 0;
+	  Mas = NULL;
+	}
+	else
+	{
+	  Size = n;
+	  Top = 0;
+	  Mas = new T[Size];
+	  for (int i = 0; i < Size; i++)
+		Mas[i] = 0;
+	}
+}
+//----------------------------------------------------------------------
+template <class T>
+TStack <T> ::TStack(TStack <T> &S)
+{
+  Size = S.Size;
+  Top = S.Top;
+  if (Size == 0)
+	Mas = NULL;
+  else
+  {
+	Mas = new T[Size];
+	for (int i = 0; i < Size; i++)
+	  Mas[i] = S.Mas[i];
+  }
+}
+//----------------------------------------------------------------------
+template <class T>
+void TStack<T> ::Put(T A)
+{
+  if (IsFull())
+	throw 1;
+  else
+  {
+	Mas[Top] = A;
+	Top++;
+  }
+}
+//----------------------------------------------------------------------
+template <class T>
+T TStack<T> ::Get()
+{
+  if (IsEmpty())
+	throw 1;
+  else
+  {
+	Top--;
+	return Mas[Top];
+  }
+}
+//----------------------------------------------------------------------
+template <class T>
+bool TStack<T> ::IsFull()
+{
+  if (Top >= Size)
+	return true;
+  else
+	return false;
+}
+//----------------------------------------------------------------------
+template <class T>
+bool TStack<T> ::IsEmpty()
+{
+  if (Top == 0)
+	return true;
+  else
+	return false;
+}
+//----------------------------------------------------------------------
