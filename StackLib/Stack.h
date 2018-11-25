857a2a4fb12bf046d55d223bba7207632be3faf2
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-11-25 05:41:07 +0300

    ch-ch-changes

--- StackLib/Stack.h
+++ StackLib/Stack.h
@@ -1,93 +1,97 @@
 #pragma once
 
+#include <iostream>
+
 template <class T>
-class TStack {
+class TStack
+{
 protected:
-	int Size;
-	int TopOne;
-	T* Mas;
+  int Size;
+  int Top;
+  T* Mas;
 public:
-	TStack(int n = 0);
-	TStack(TStack<T> &A);
-	void PutSmth(T A);
-	T GetSmth();
-	bool IsFull();
-	bool IsEmpty();
+  TStack(int n = 0);
+  TStack(TStack<T> &S);
+  T Get();
+  void Put(T A);
+  bool IsFull();
+  bool IsEmpty();
 };
-//-----------------------------------------------------------
+//----------------------------------------------------------------------
 template <class T>
-TStack <T> :: TStack(int n) 
+TStack <T> ::TStack(int n)
 {
-	if (n < 0) throw "wrong";
-	else 
-		if (n == 0) 
-		{
-			Size = TopOne = 0;
-			Mas = NULL;
-		}
-		else 
-		{
-			Size = n;
-			TopOne = 0;
-			Mas = new T[Size];
-			for (int i = 0; i < Size; i++)
-				Mas[i] = 0;
-		}
-}
-//-----------------------------------------------------------
-template <class T>
-TStack <T> :: TStack(TStack <T> &A) 
-{
-	Size = A.Size;
-	Top = A.TopOne;
-	if (Size == 0)
-		Mas = NULL;
-	else 
+  if (n < 0) throw 1;
+  else
+	if (n == 0)
 	{
-		Mas = new T[Size];
-		for (int i = 0; i < Size; i++)
-			Mas[i] = A.Mas[i];
+	  Size = Top = 0;
+	  Mas = NULL;
 	}
-}
-//-----------------------------------------------------------
-template <class T>
-void TStack<T> :: PutSmth(T A) 
-{
-	if (IsFull())
-		throw "wrong";
-	else 
-	{
-		Mas[TopOne] = A;
-		Top++;
-	}
-}
-//-----------------------------------------------------------
-template <class T>
-T TStack<T> :: GetSmth() 
-{
-	if (IsEmpty()) 
-		throw "wrong";
-	else 
-	{
-		TopOne--;
-		return Mas[Top + 1];
-	}
-}
-//-----------------------------------------------------------
-template <class T>
-bool TStack<T> :: IsFull() 
-{
-	if (TopOne >= Size)
-		return true;
 	else
-		return false;
+	{
+	  Size = n;
+	  Top = 0;
+	  Mas = new T[Size];
+	  for (int i = 0; i < Size; i++)
+		Mas[i] = 0;
+	}
 }
-//-----------------------------------------------------------
+//----------------------------------------------------------------------
 template <class T>
-bool TStack<T> :: IsEmpty() 
+TStack <T> ::TStack(TStack <T> &S)
 {
-	if (TopOne == 0)
-		return true;
-	else
-		return false;
+  Size = S.Size;
+  Top = S.Top;
+  if (Size == 0)
+	Mas = NULL;
+  else
+  {
+	Mas = new T[Size];
+	for (int i = 0; i < Size; i++)
+	  Mas[i] = S.Mas[i];
+  }
 }
+//----------------------------------------------------------------------
+template <class T>
+void TStack<T> ::Put(T A)
+{
+  if (IsFull())
+	throw 1;
+  else
+  {
+	Mas[Top] = A;
+	Top++;
+  }
+}
+//----------------------------------------------------------------------
+template <class T>
+T TStack<T> ::Get()
+{
+  if (IsEmpty())
+	throw 1;
+  else
+  {
+	Top--;
+	return Mas[Top];
+  }
+}
+//----------------------------------------------------------------------
+template <class T>
+bool TStack<T> ::IsFull()
+{
+  if (Top >= Size)
+	return true;
+  else
+	return false;
+}
+//----------------------------------------------------------------------
+template <class T>
+bool TStack<T> ::IsEmpty()
+{
+  if (Top == 0)
+	return true;
+  else
+	return false;
+}
+//----------------------------------------------------------------------
