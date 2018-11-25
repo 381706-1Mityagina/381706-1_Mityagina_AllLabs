774370eba6b299e43575b337da0ae04621e100bf
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-11-25 03:55:45 +0300

    trying to make it work

---
+++ Stack/main_Stack.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "..//Stack/Stack.h"
+
+using namespace std;
+
+int main()
+{
+  cout << "An example of using the Stack will be implemented here" << endl;
+  
+  /* Something interesting */
+  
+  return 0;
+}
