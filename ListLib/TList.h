5da4ecfa21a3e91106af5293e0aeeb1c71d67380
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-04 22:50:28 +0300

    idk

--- ListLib/TList.h
+++ ListLib/TList.h
@@ -8,30 +8,42 @@ class TList
 {
 protected:
 	TElement <T>* begin;
+	int size;
 public:
 	TList(); 
 	TList(TList<T> &List); 
 	virtual ~TList();
 
+	int GetSize();
 	void PutBegin(T A); 
+	void PutCurrent(T A);
 	void PutEnd(T A); 
 	T GetBegin(); 
+	T GetCurrent(); 
+	T GetEnd(); 
+
 				
 	bool IsFull();
 	bool IsEmpty();
+	
+	virtual void DelFirst(void);
+	virtual void DelCurrent(void);
+	virtual void DelList(void);
+
 };
 
 //-----------------------------------------------------------
 template <class T>
 TList<T>::TList()
 {
+	size = 0;
 	begin = 0;
 }
 //-----------------------------------------------------------
 template <class T>
 TList<T>::TList(TList<T> &List)
 {
-	TElement<T>* A = List.Begin, B;
+	TElement<T>* A = List.begin, B;
 	if (List.begin == 0)
 		begin = 0;
 	else {
@@ -60,7 +72,8 @@ TList<T>::~TList()
 template <class T>
 void TList<T>::PutBegin(T A)
 {
-	if (begin == 0) {
+	if (begin == 0) 
+	{
 		TElement <T>* temporary = new TElement <T>(A, 0);
 		begin = temporary;
 	}
@@ -69,14 +82,73 @@ void TList<T>::PutBegin(T A)
 		TElement <T>* temporary = new TElement <T>(A, begin);
 		begin = temporary;
 	}
+	size++;
+}
+//-----------------------------------------------------------
+template <class T>
+void TList<T>::PutCurrent(T A)
+{
+	
+}
+//-----------------------------------------------------------
+template <class T>
+void TList<T>::DelFirst()
+{
+   if (size > 0)
+	{
+		TElement<T>* temporary = begin->next;
+		T result= begin->data;
+		delete begin;
+		begin = temporary;
+		size--;
+		return result;
+	}
+	else
+		throw "Error";	
+}
+//-----------------------------------------------------------
+template <class T>
+void TList<T>::DelCurrent()
+{
+	
+}
+//-----------------------------------------------------------
+template <class T>
+void TList<T>::DelEnd()
+{
+if (size == 0)
+	{
+		throw 1;
+	}
+	else 
+		if (size == 1)
+		{
+			TList<T> B = this;
+			while (B -> GetNext() != 0)
+			B = B->GetNext();
+		        
+			head = B = NULL;
+			size--;
+		}
+		else
+		{
+			TElement<T>* temporary = begin;
+			while (temporary->next->next)
+			{
+				temporary = temporary->next;
+			}
+			temporary->next = NULL;
+			size--;
+		}	
 }
 //-----------------------------------------------------------
 template <class T>
 void TList<T>::PutEnd(T A)
 {
-	if (begin != 0) {
+	if (begin != 0) 
+	{
 		TElement <T> *B = begin;
-		while (A->GetNext() != 0)
+		while (B->GetNext() != 0)
 			B = B->GetNext();
 		B->SetNext(new TElement <T>(A, 0));
 	}
@@ -100,6 +172,18 @@ T TList<T>::GetBegin()
 }
 //-----------------------------------------------------------
 template <class T>
+T TList<T>::GetCurrent(T A)
+{
+	
+}
+//-----------------------------------------------------------
+template <class T>
+T TList<T>::GetEnd(T A)
+{
+	
+}
+//-----------------------------------------------------------
+template <class T>
 bool TList<T>::IsFull()
 {
 	try
@@ -107,7 +191,8 @@ bool TList<T>::IsFull()
 		TElement<T>* A = new TElement<T>();
 		if (A == 0)
 			return false;
-		else {
+		else 
+		{
 			delete A;
 			return true;
 		}
@@ -123,7 +208,8 @@ bool TList<T>::IsFull()
 template <class T>
 bool TList<T>::IsEmpty()
 {
-	if (begin == 0)
+	//if (begin == 0)
+	if (size == 0)
 		return true;
 	else
 		return false;
