18fe0ac4fb89001b63103b8319fe5096df5b5152
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-05 00:42:54 +0300

    Turn and face the strange ch-ch-changes

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-  float a[3] = { 0.6, 5.7, 0.4 };
+  /*float a[3] = { 0.6, 5.7, 0.4 };
 	float b[3] = { 0.85, 1.3, 5.8 };
 	int c[3] = { 4, 5, 7 };
   
@@ -18,6 +18,6 @@ int main()
 	TVector<float> B(b, 3);
  	TVector<int> C(c, 3);
 	
-	//std::cout << A << "\n" << B << "\n" << C << "\n" << "\n";
+	//std::cout << A << "\n" << B << "\n" << C << "\n" << "\n";*/
 return 0;
 }
