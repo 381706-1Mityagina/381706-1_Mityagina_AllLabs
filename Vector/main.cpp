c747877b3626c95c6a2e6fabd70c0956b2340f57
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-07 22:10:34 +0300

    trn nd fc th strng chngs

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -7,17 +7,22 @@ using namespace std;
 
 int main()
 {
-  /*float a[3] = { 0.6, 5.7, 0.4 };
-	float b[3] = { 0.85, 1.3, 5.8 };
-	int c[3] = { 4, 5, 7 };
+	cout << "\t <<< An example of using the Vector will be implemented here >>>" << "\n\n";
+  double a[] = { 0.6, 5.7, 0.4 };
+	double b[] = { 0.85, 1.3, 5.8 };
+	int c[] = { 4, 5, 7 };
   
-       TVector<int> Sum;
-	TVector<float> Razn;
-	TVector<float> Umn;
-        TVector<float> A(a, 3);
-	TVector<float> B(b, 3);
+  TVector<int> Sum;
+	TVector<double> Razn;
+  TVector<double> A(a, 3);
+	TVector<double> B(b, 3);
  	TVector<int> C(c, 3);
+
+	Razn = B - A;
+	Sum = C + C;
+	Sum++;
 	
-	//std::cout << A << "\n" << B << "\n" << C << "\n" << "\n";*/
+	std::cout << A << "\n" << B << "\n" << C << "\n" << Razn << "\n" << Sum << "\n";
 return 0;
 }
+
