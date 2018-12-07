4c3ff431315f562afed945721c03c005a255e633
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-07 22:14:20 +0300

    trn nd fc th strng chngs

--- Matrix/main_Matrix.cpp
+++ Matrix/main_Matrix.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //#pragma argsused
 int main(int argc, char* argv[]) 
 {
-  cout << "An example of using the Matrix will be implemented here" << endl;
+  cout << "\t <<< An example of using the Matrix will be implemented here >>>" << endl;
   TMatrix <int> Matr1(5), Matr2(5), Matr3(5), Result1(5), Result2(5), Result3(5);
   int i, j;
   for (i = 0; i < 5; i++)
@@ -17,16 +17,15 @@ int main(int argc, char* argv[])
 	  Matr2[i][j] = (i * 10 + j + 3 + i) * 100;
 	  Matr3[i][j] = i + j;
 	}
+	
   Result1 = Matr1 + Matr2;
   Result2 = Matr1 - Matr2;
- // Result3 = Matr1 * Matr3;
 
-  cout << "\t || Matr1 ||" << endl << Matr1 << endl;
-  cout << "\t || Matr2 ||" << endl << Matr2 << endl;
-  cout << "\t || Matr3 ||" << endl << Matr3 << endl;
-  cout << "\t ||Result1 ||" << endl << Result1 << endl;
-  cout << "\t ||Result2 ||" << endl << Result2 << endl;
- // cout << "\t ||Result3 ||" << endl << Result3 << endl;
+  cout << "\t || Matr1 ||" << "\n" << Matr1 << "\n";
+  cout << "\t || Matr2 ||" << "\n" << Matr2 << "\n";
+  cout << "\t || Matr3 ||" << "\n" << Matr3 << "\n";
+  cout << "\t ||Result1 ||" << "\n" << Result1 << "\n";
+  cout << "\t ||Result2 ||" << "\n" << Result2 << "\n";
 
   //system("pause");  
   return 0;
