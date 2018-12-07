8af9ee90af081bd1842f100f985e28579225234d
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-07 22:53:44 +0300

    trn nd fc th strng chngs

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	cout << "\t <<< An example of using the Vector will be implemented here >>>" << "\n\n";
   double a[] = { 0.6, 5.7, 0.4 };
 	double b[] = { 0.85, 1.3, 5.8 };
@@ -23,6 +24,18 @@ int main()
 	Sum++;
 	
 	std::cout << A << "\n" << B << "\n" << C << "\n" << Razn << "\n" << Sum << "\n";
+
+	// Проверка работы операторов ввода-вывода
+	cout << "\n\t<<< Ввод вектора >>>\n";
+	cout << "Введите размер вектора\n";
+	int n;
+	cin >> n;
+	TVector<int> _vector(n);
+	cout << "Введите элементы вектора\n";
+	cin >> _vector;
+	cout << "\nВывод элементов вектора\n";
+	cout << _vector << "\n";
+	
 return 0;
 }
 
