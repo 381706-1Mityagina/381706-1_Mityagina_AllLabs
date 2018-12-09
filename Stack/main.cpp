3459edd06058a3ac09bfdc54eb9ce9a3382060e3
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-09 19:50:44 +0300

    DONE )

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
-#include "Stack.h"
+#include "..//StackLib/Stack.h"
 
 using namespace std;
 
 int main()
-{
+{	
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+	cout << "Введите кол-во элементов в стеке\n";
+	cin >> n;
+	TStack<char> symbolStack(n);
+	char symbol;
+	cout << "Введите элементы стека\n";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> symbol;
+		symbolStack.Put(symbol); // помещаем элементы в стек
+	}
+
+	cout << "\n";
+
+	symbolStack.PrintStack(); // печать стека
+
+	cout << "\n\nУдалим элемент из стека\n";
+	symbolStack.Get();
+
+	symbolStack.PrintStack(); // печать стека
+
+	TStack<char> newOne(symbolStack);
+
+	cout << "\n\nКонструктор копирования в действии:\n";
+	newOne.PrintStack();
+
 	return 0;
 }
