7500002dd6db86d95013e74603fd3b76cbcc3405
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-12 00:23:15 +0300

    2nd step

--- Queue/main1.cpp
+++ Queue/main1.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <locale.h>
 #include "Queue.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[]) 
 {
-	return 0;
-}
+ setlocale(LC_ALL, "Russian");
+ TQueue test;
+ int temporary;
+ cout << "Тестирование программ поддержки структуры типа очереди" << endl;
+ for ( int i=0; i < 35; i++ ) 
+ {
+ test.Put(i);
+ cout << "Положили значение " << i << endl;
+ }
+ getch();
+ while ( !test.IsEmpty() ) {
+ temp = test.Get();
+ cout << "Взяли значение " << temp << endl;
+ }
+ cout << "Нажмите любую клавишу" << endl;
+ getch();
+ return 0;
+} 
