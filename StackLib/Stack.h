bbc5e89b3afbd1aef63fc892ab5a8a0d2c899d63
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-12 00:39:31 +0300

    8 step

--- StackLib/Stack.h
+++ StackLib/Stack.h
@@ -1,27 +1,36 @@
 #pragma once
 
 #include <iostream>
+#include "..//Exception/Exception.h"
+
+using namespace std;
 
 template <class T>
 class TStack
 {
 protected:
-  int Size;
-  int Top;
-  T* Mas;
+  int Size; // Размер стека
+  int Top; // элемент,расположенный на вершине стека
+  T* Mas; // элементы стека
 public:
-  TStack(int n = 0);
-  TStack(TStack<T> &S);
-  T Get();
-  void Put(T A);
-  bool IsFull();
-  bool IsEmpty();
+  int GetSize() { return Size; } // возвращает размер
+  TStack(int n = 0); // конструктор инициализации
+  TStack(TStack<T> &S); // конструктор копирования
+  T Get(); // возвращает элемент, расположенный на вершине стека
+  void Put(T A); // положить элемент
+  bool IsFull(); // проверка на полноту
+  bool IsEmpty(); // проверка на пустоту
+  void PrintStack(); // вывод стека на экран
+
+  int operator!=(const TStack<T>& stack) const; // проверка на неравенство
+  int operator==(const TStack<T>& stack) const; //проверка на равенство
+  TStack& operator=(const TStack<T>& stack); // приравнивание
 };
 //----------------------------------------------------------------------
 template <class T>
 TStack <T> ::TStack(int n)
 {
-  if (n < 0) throw 1;
+  if (n < 0) throw TException(" Negative size.");
   else
 	if (n == 0)
 	{
@@ -57,7 +66,7 @@ template <class T>
 void TStack<T> ::Put(T A)
 {
   if (IsFull())
-	throw 1;
+	throw TException("Stack is full");
   else
   {
 	Mas[Top] = A;
@@ -69,7 +78,7 @@ template <class T>
 T TStack<T> ::Get()
 {
   if (IsEmpty())
-	throw 1;
+	throw TException("Stack is empty");
   else
   {
 	Top--;
@@ -93,5 +102,54 @@ bool TStack<T> ::IsEmpty()
 	return true;
   else
 	return false;
+}
+//----------------------------------------------------------------------
+template <class T>
+TStack<T>& TStack<T> ::operator=(const TStack<T>& stack) 
+{
+		if (this != &stack)
+		{
+			if (Top != stack.Top) 
+			{
+				Top = stack.Top;
+				delete[] Mas;
+				Mas = new T[Top];
+			}
+			Size = stack.Size;
+			for (int i = 0; i < Size; i++) 
+			{
+				Mas[i] = stack.Mas[i];
+			}
+		}
+		return *this;
+	}
+
+//----------------------------------------------------------------------
+template <class T>
+int TStack<T> ::operator==(const TStack<T>& stack) const 
+{
+		if (Top != stack.Top)
+			return 0;
+		if (Size != stack.Size)
+			return 0;
+		for (int i = 0; i < Size; i++) 
+		{
+			if (Mas[i] != stack.Mas[i])
+				return 0;
+		}
+		return 1;
+}
+//----------------------------------------------------------------------
+template <class T>
+int TStack<T> ::operator!=(const TStack<T>& stack) const 
+	{
+		return !(*this == stack);
+	}
+//----------------------------------------------------------------------
+template <class T>
+void TStack<T>:: PrintStack()
+{
+	for (int i = Top - 1; i >= 0; i--)
+		cout << "\t|" << Mas[i] << "|" << endl;
 }
 //----------------------------------------------------------------------
