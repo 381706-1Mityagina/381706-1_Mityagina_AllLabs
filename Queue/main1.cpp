2570df2d1cfa04d0dbcbd5af1ec96ebe7f26ae26
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-12 05:53:35 +0300

    ?

--- Queue/main1.cpp
+++ Queue/main1.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
-#include <locale.h>
 #include "..//QueueLib/Queue.h"
 
 using namespace std;
 
-int main()
+int main( )
 {
- setlocale(LC_ALL, "Russian");  
  TQueue<int> test;
  int temporary;
- cout << "Тестирование программ поддержки структуры типа очереди" << endl;
- for ( int i = 0; i < 35; i++ ) 
+ cout << "Testing Queue Type Structure Support Programs" << endl;
+  
+ for ( int i = 0; i < 10; i++ ) 
  {
  while (!test.IsFull())
-     test.Put(i);
- cout << "Положили значение " << i << endl;
+   test.Put(i);
+ cout << "\tPut value  " << i << endl;
  }
- while (!test.IsEmpty()) 
- {
- temporary = test.Get();
- cout << "Взяли значение " << temporary << endl;
- }
+ cout << "\tGet 1 value:" << test.Get() << endl;
+ cout << "\tGet 2 value:" << test.Get() << endl;
+ cout << "\tGet 3 value:" << test.Get() << endl;
+ cout << endl;
+ 
+ TQueue<int> test1(10);
+ test1.Put(43);
+ test1.Put(42);
+ test1.Put(41);
+ cout << "\tGet 1 value:" << test1.Get() << endl;
+ cout << "\tGet 2 value:" << test1.Get() << endl;
+
  return 0;
 }  
