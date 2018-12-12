e51e47cb12a14dbc684fcac412213694a6aefd8c
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-12 06:58:45 +0300

    TQueue

--- QueueTest/Queue_Test.cpp
+++ QueueTest/Queue_Test.cpp
@@ -2,17 +2,17 @@
 
 #include "gtest.h"
 
-TEST(Queue, can_create_queue_with_positive_size)
+TEST(TQueue, can_create_queue_with_positive_size)
 {
 	ASSERT_NO_THROW(TQueue <int> V(7));
 }
 //------------------------------------------------------------------------
-TEST(Queue, cannot_create_queue_with_negative_size)
+TEST(TQueue, cannot_create_queue_with_negative_size)
 {
 	ASSERT_ANY_THROW(TQueue <int> V(-7));
 }
 //------------------------------------------------------------------------
-TEST(Queue, can_do_stuff_like_put_and_get)
+TEST(TQueue, can_do_stuff_like_put_and_get)
 {
 	TQueue <int> Q(1);
 	
@@ -21,7 +21,7 @@ TEST(Queue, can_do_stuff_like_put_and_get)
 	ASSERT_EQ(7, Q.Get());
 }
 //------------------------------------------------------------------------
-TEST(Queue, can_tell_if_Queue_is_not_full)
+TEST(TQueue, can_tell_if_Queue_is_not_full)
 {
 	TQueue <int> Q(7);
 	
