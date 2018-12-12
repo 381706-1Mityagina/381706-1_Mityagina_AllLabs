2f26023c5a27cf8ec44c40c5da8b79e240625ba6
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-12 08:44:08 +0300

    changes

--- Queue/main1.cpp
+++ Queue/main1.cpp
@@ -6,24 +6,25 @@ using namespace std;
 int main( )
 {
  TQueue<int> test(10);
- int temporary;
- cout << "Testing Queue Type Structure Support Programs" << endl;
-  
- for ( int i = 0; i < 10; i++ ) 
- {
- if (!test.IsFull())
-  test.Put(i);
- cout << "\tPut value  " << i << endl;
- cout << "\tGet value: " << test.Get() << endl << endl;
- }
- cout << endl;
- 
- TQueue<int> test1(10);
- test1.Put(55);
- test1.Put(44);
- test1.Put(66);
- cout << "\tGet 1 value:" << test1.Get() << endl;
- cout << "\tGet 2 value:" << test1.Get() << endl;
+	int temporary;
+	cout << "\n\tTesting Queue Type Structure Support Programs" << endl;
 
- return 0;
+	for (int i = 0; i < 10; i++)
+	{
+		if (!test.IsFull())
+			test.Put(i);
+		cout << "\t\tPut value  " << i << endl;
+		cout << "\t\tGet value: " << test.Get() << endl << endl;
+	}
+	cout << endl;
+
+	TQueue<int> test1(10);
+	test1.Put(55);
+	test1.Put(44);
+	test1.Put(66);
+	cout << "\t\tGet 1 value:" << test1.Get() << endl;
+	cout << "\t\tGet 2 value:" << test1.Get() << endl;
+	cout << endl;
+
+	return 0;
 }  
