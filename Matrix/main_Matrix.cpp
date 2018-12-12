78d18f8ae54b10c3a7aee39b10bec54933206111
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-13 02:35:37 +0300

    ??

--- Matrix/main_Matrix.cpp
+++ Matrix/main_Matrix.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 {
   setlocale(LC_ALL, "Russian");
   cout << "\t <<< An example of using the Matrix will be implemented here >>>" << "\n\n";
-  TMatrix <int> Matr1(5), Matr2(5), Matr3(5), Result1(5), Result2(5), Result3(5), Result4(5);
+  TMatrix <int> Matr1(5), Matr2(5), Matr3(5), Matr4(5), Result1(5), Result2(5), Result3(5), Result4(5);
   int i, j;
   for (i = 0; i < 5; i++)
 	for (j = 0; j < 5 - i; j++) 
@@ -18,11 +18,12 @@ int main(int argc, char* argv[])
 	  Matr1[i][j] = (i + 1) * (j + 1);
 	  Matr2[i][j] = (i * 10 + j + 3 + i) * 100;
 	  Matr3[i][j] = i + j;
+	  Matr4[i][j] = 2*i + 2*j;
 	}
   Result1 = Matr1 + Matr2;
   Result2 = Matr1 - Matr2;
   Result3 = Matr3 * Matr3;
-  Result4 = Matr3.InverseMatrix();
+  Result4 = Matr3/Matr4;
 
   cout << "\t || Matr1 ||" << endl << Matr1 << endl;
   cout << "\t || Matr2 ||" << endl << Matr2 << endl;
