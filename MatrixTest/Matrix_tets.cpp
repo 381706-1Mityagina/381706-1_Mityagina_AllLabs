de5a998fd8aecde018fb358aaa4dbd17e0d44e79
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-17 22:52:09 +0300

    kind of works

--- MatrixTest/Matrix_tets.cpp
+++ MatrixTest/Matrix_tets.cpp
@@ -5,26 +5,26 @@ TEST(TMatrix, can_create_matrix_with_positive_length)
 {
   ASSERT_NO_THROW(TMatrix<int> m(7));
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, throws_when_create_matrix_with_negative_length)
 {
   ASSERT_ANY_THROW(TMatrix<int> m(-7));
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, can_create_copied_matrix)
 {
   TMatrix<int> m(7);
 
   ASSERT_NO_THROW(TMatrix<int> m1(m));
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, can_get_size)
 {
   TMatrix<int> m(7);
  
   EXPECT_EQ(7, m.GetDlina());
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, can_set_and_get_element)
 {
   TMatrix<int> m(7);
@@ -33,21 +33,21 @@ TEST(TMatrix, can_set_and_get_element)
 
   EXPECT_EQ(7, m[1][2]);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, throws_when_set_element_with_negative_index)
 {
   TMatrix<int> m(7);
 
   ASSERT_ANY_THROW(m[-2][1] = 1);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, throws_when_set_element_with_too_large_index)
 {
   TMatrix<int> m(7);
 
   ASSERT_ANY_THROW(m[9][1] = 1);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, can_assign_matrix_to_itself)
 {
   TMatrix<int> m1(7);
@@ -57,7 +57,7 @@ TEST(TMatrix, can_assign_matrix_to_itself)
   
   EXPECT_EQ(m1, m1);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, can_assign_matrices_of_equal_size)
 {
   TMatrix<int> m1(7);
@@ -70,7 +70,7 @@ TEST(TMatrix, can_assign_matrices_of_equal_size)
   
   EXPECT_EQ(m3, m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, assign_operator_change_matrix_size)
 {
   TMatrix<int> m1(7);
@@ -81,7 +81,7 @@ TEST(TMatrix, assign_operator_change_matrix_size)
   
   EXPECT_EQ(7, m2.GetDlina());
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, can_add_matrices_with_equal_size)
 {
   TMatrix<int> m1(3);
@@ -94,7 +94,7 @@ TEST(TMatrix, can_add_matrices_with_equal_size)
 
   EXPECT_EQ(m3, m1 + m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, cant_add_matrices_with_not_equal_size)
 {
   TMatrix<int> m1(7);
@@ -106,7 +106,7 @@ TEST(TMatrix, cant_add_matrices_with_not_equal_size)
 
   ASSERT_ANY_THROW(m1 + m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, can_subtract_matrices_with_equal_size)
 {
 	TMatrix <int> m1(4);
@@ -119,7 +119,7 @@ TEST(TMatrix, can_subtract_matrices_with_equal_size)
 
 	EXPECT_EQ(m3[1][2], -2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, cannot_subtract_matrixes_with_not_equal_size)
 {
 	TMatrix <int> m1(4);
@@ -127,7 +127,7 @@ TEST(TMatrix, cannot_subtract_matrixes_with_not_equal_size)
 
 	ASSERT_ANY_THROW(m1 - m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, matrixes_are_not_equal_if_are_not_equal)
 {
 	TMatrix<int> m1(4);
@@ -135,7 +135,7 @@ TEST(TMatrix, matrixes_are_not_equal_if_are_not_equal)
 
 	ASSERT_TRUE(m1 != m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, An_equality_matrix_false_chack)
 {
   TMatrix <int> m1(3), m2(3);
@@ -149,14 +149,14 @@ TEST(TMatrix, An_equality_matrix_false_chack)
 	
   ASSERT_FALSE(m1 == m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, An_equality_matrix_with_diff_size)
 {
   TMatrix <int> m1(8), m2(3);
 	
   ASSERT_FALSE(m1 == m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, An_inequality_matrix_true_check)
 {
   TMatrix <int> m1(7), m2(7);
@@ -170,7 +170,7 @@ TEST(TMatrix, An_inequality_matrix_true_check)
 	
   ASSERT_TRUE(m1 != m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, multiplication_is_impossible_if_size1_is_not_equal_to_size2)
 {
 	TMatrix<int> m1(4);
@@ -178,7 +178,7 @@ TEST(TMatrix, multiplication_is_impossible_if_size1_is_not_equal_to_size2)
 
 	ASSERT_ANY_THROW(m1 * m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, summ_is_correct)
 {
 	TMatrix<int> m1(2), m2(2), m(2);
@@ -192,21 +192,21 @@ TEST(TMatrix, summ_is_correct)
 		}
 	 EXPECT_EQ(m, m1+ m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, thorws_when_division_matrix_with_diff_size)
 {
 	TMatrix <int> m1(7), m2(8);
 
 	ASSERT_ANY_THROW(m1 / m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, thorws_when_division_matrix_with_det_eq_zero)
 {
 	TMatrix <int> m1(2), m2(2);
 
 	ASSERT_ANY_THROW(m2 / m1);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, is_able_to_divide_matrix)
 {
 	TMatrix<int> m1(2), m2(2), res(2), smth(2);
@@ -226,7 +226,7 @@ TEST(TMatrix, is_able_to_divide_matrix)
 	smth = m1 / m2;
 	EXPECT_EQ(res, smth);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, razn_is_correct)
 {
 	TMatrix<int> m1(2), m2(2), m(2);
@@ -240,7 +240,7 @@ TEST(TMatrix, razn_is_correct)
 		}
 	 EXPECT_EQ(m, m1 - m2);
 }
-
+//-----------------------------------------------------------------------
 TEST(TMatrix, can_multiplicate_matrix)
 {
 	TMatrix <int> m1(2), m2(2), res(2);
@@ -259,3 +259,4 @@ TEST(TMatrix, can_multiplicate_matrix)
 	
 	ASSERT_TRUE(res == m1 * m2);
 }
+//-----------------------------------------------------------------------
