750cd1ee347bf8c2e2e079f841469559bed5769a
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-19 18:26:14 +0300

    Update main_ArrayList.cpp

--- ArrayList/main_ArrayList.cpp
+++ ArrayList/main_ArrayList.cpp
@@ -5,9 +5,9 @@ int main()
 {
 	std::cout << "An example of using the List will be implemented here" << std::endl;
 	TArrayList <int> L1;
-	L1.PutBegin(60);
-	L1.PutBegin(40);
-	std::cout << L1.GetBegin();
+	L1.PutStart(60);
+	L1.PutStart(40);
+	std::cout << L1.GetStart();
 	std::cout << "\n";
 	//system("pause");
   return 0;
