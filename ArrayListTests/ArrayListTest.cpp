1126e718d201466b14addf96b2707b6289678d57
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-19 19:06:33 +0300

    +

--- ArrayListTests/ArrayListTest.cpp
+++ ArrayListTests/ArrayListTest.cpp
@@ -1,34 +1,34 @@
 #include <gtest.h>
 #include "..//ArrayListLib/TArrayList.h"
 
-TEST(TArrList, can_create_list)
+TEST(TArrayList, can_create_list)
 {
-	ASSERT_NO_THROW(TArrList <int> List());
+	ASSERT_NO_THROW(TArrayList <int> List());
 }
 
 
-TEST(TArrList, can_create_list_with_positive_size)
+TEST(TArrayList, can_create_list_with_positive_size)
 {
-	ASSERT_NO_THROW(TArrList <int> List(3));
+	ASSERT_NO_THROW(TArrayList <int> List(3));
 }
 
-TEST(TArrList, throws_when_create_list_with_negative_size)
+TEST(TArrayList, throws_when_create_list_with_negative_size)
 {
-	ASSERT_ANY_THROW(TArrList <int> List(-1));
+	ASSERT_ANY_THROW(TArrayList <int> List(-1));
 }
 
-TEST(TArrList, can_push_and_pull_beginning)
+TEST(TArrayList, can_push_and_pull_beginning)
 {
-	TArrList <int> List(1);
+	TArrayList <int> List(1);
 
 	List.PutBegin(7);
 	
 	ASSERT_EQ(7, List.GetBegin());
 }
 
-TEST(TArrList, can_push_and_pull_end)
+TEST(TArrayList, can_push_and_pull_end)
 {
-	TArrList <int> List(3);
+	TArrayList <int> List(3);
 	
 	List.PutEnd(2);
 	List.PutEnd(4);
@@ -38,25 +38,25 @@ TEST(TArrList, can_push_and_pull_end)
 }
 
 
-TEST(TArrList, full_check_false)
+TEST(TArrayList, full_check_false)
 {
-	TArrList <int> List(1);
+	TArrayList <int> List(1);
 	
 	ASSERT_FALSE(List.IsFull());
 }
 
-TEST(TArrList, full_check_true)
+TEST(TArrayList, full_check_true)
 {
-	TArrList <int> List(1);
+	TArrayList <int> List(1);
 	
 	List.PutBegin(7);
 	
 	ASSERT_TRUE(List.IsFull());
 }
 
-TEST(TArrList, can_use_free_elements)
+TEST(TArrayList, can_use_free_elements)
 {
-	TArrList <int> List(5);
+	TArrayList <int> List(5);
 
 	List.PutBegin(6); //6
 	List.PutBegin(7); //7 6
@@ -65,25 +65,25 @@ TEST(TArrList, can_use_free_elements)
 	ASSERT_EQ(7, List.GetBegin()); //6 3
 }
 
-TEST(TArrList, empty_check_false)
+TEST(TArrayList, empty_check_false)
 {
-	TArrList <int> List(1);
+	TArrayList <int> List(1);
 	
 	List.PutBegin(7);
 	
 	ASSERT_FALSE(List.IsEmpty());
 }
 
-TEST(TArrList, empty_check_true)
+TEST(TArrayList, empty_check_true)
 {
-	TArrList <int> List(1);
+	TArrayList <int> List(1);
 	
 	ASSERT_TRUE(List.IsEmpty());
 }
 
-TEST(TArrList, throws_when_push_begin_or_end_elem_in_full_list)
+TEST(TArrayList, throws_when_push_begin_or_end_elem_in_full_list)
 {
-	TArrList <int> List(1);
+	TArrayList <int> List(1);
 	
 	List.PutBegin(7);
 	
@@ -91,9 +91,9 @@ TEST(TArrList, throws_when_push_begin_or_end_elem_in_full_list)
 	ASSERT_ANY_THROW(List.PutFinish(2));
 }
 
-TEST(TArrList, throws_when_pull_begin_or_end_elem_in_empty_list)
+TEST(TArrayList, throws_when_pull_begin_or_end_elem_in_empty_list)
 {
-	TArrList <int> List(1);
+	TArrayList <int> List(1);
 	
 	ASSERT_ANY_THROW(List.GetBegin());
 	ASSERT_ANY_THROW(List.GetEnd());
