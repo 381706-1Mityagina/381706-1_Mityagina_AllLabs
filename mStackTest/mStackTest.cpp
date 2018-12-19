0d38f12919cbec7d0134ff8be0008bc4a3262088
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-19 12:44:00 +0300

    ?

---
+++ mStackTest/mStackTest.cpp
@@ -0,0 +1,27 @@
+#include "..//multiLib/multi.h"
+
+#include <gtest.h>
+
+TEST(TMStack, can_create_multistack)
+{
+	ASSERT_NO_THROW(TMStack<int> lost);
+}
+
+TEST(TMStack, can_create_copy)
+{
+	TMStack<int> mst;
+
+	ASSERT_NO_THROW(TMStack<int> mst1(mst));
+}
+
+TEST(TMStack, cannot_create_multictack_with_negative_size)
+{
+	ASSERT_ANY_THROW(TMStack<int> mst(0, -1));
+}
+
+TEST(TMStack, can_get_size)
+{
+	TMStack<int> mst;
+
+	ASSERT_NO_THROW(mst.GetSize);
+}
