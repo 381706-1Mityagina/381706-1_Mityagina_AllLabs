57ac47dc38010e06b7cfa0172edc365cd76f195e
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-19 20:26:47 +0300

    Create StackList.cpp

---
+++ StackList/StackList.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "../StackListLib/StackList.h"
+
+using namespace std;
+
+int main()
+{
+  cout << "An example of using the StackList will be implemented here" << endl;
+  
+  /* smth interesting */
+  
+  return 0;
+}
