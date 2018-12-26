dac5f9bb04d1308999c9cb08d97215476728e583
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-26 04:09:05 +0300

    ch-ch-changes

--- ListTests/List_Tests.cpp
+++ ListTests/List_Tests.cpp
@@ -25,72 +25,45 @@ TEST(TList, put_begin_has_right_answer)
 {
 	TList<int> lost1;
 	
-  lost1.PutBegin(5);
+ ASSERT_NO_THROW (lost1.AddFirst(5));
   
-	EXPECT_EQ (5, lost1.GetBegin());
 }
 
 TEST(TList, can_put_end)
 {
 	TList<int> lost;
   
-	ASSERT_NO_THROW (lost.PutEnd(5));
-}
-
-TEST(TList, throws_when_get_end_from_empty_list)
-{
-	TList<int> lost;
-  
-	ASSERT_ANY_THROW (int a = lost.GetEnd());
+	ASSERT_NO_THROW (lost.AddLast(5));
 }
 
 TEST (TList, isfull_gives_right_answer)
 {
 	TList<int> lost;
   
-	lost.PutEnd(8);
+	lost.AddLast(8);
 	
-  EXPECT_EQ (true, lost.IsFull());
+  EXPECT_EQ (true, lost.isEnd());
 }
 
 TEST (TList, isempty_gives_right_answer)
 {
 	TList<int> lost;
 	
-  EXPECT_EQ (true, lost.IsEmpty());
+  EXPECT_EQ (true, lost.isEmpty());
 }
 
 TEST(TList, can_put_begin)
 {
 	TList<int> lost;
 	
-  ASSERT_NO_THROW (lost.PutBegin(5));
-}
-
-TEST(TList, can_get_begin)
-{
-	TList<int> lost;
-	
-  lost.PutBegin(3);
-	
-  ASSERT_NO_THROW (int a = lost.GetBegin());
+  ASSERT_NO_THROW (lost.AddFirst(5));
 }
 
 TEST(TList, can_check_empty_true)
 {
 	TList<int> lost;
 
-	ASSERT_TRUE(lost.IsEmpty());
-}
-
-TEST(TList, can_get_end)
-{
-	TList<int> lost;
-
-	lost.PutBegin(7);
-	lost.PutBegin(6);
-
-	ASSERT_EQ(lost.GetEnd(), 7);
+	ASSERT_TRUE(lost.isEmpty());
 }
 
 TEST(TElement, can_get_data)
