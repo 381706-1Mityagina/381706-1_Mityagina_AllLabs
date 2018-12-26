c783931fe55b0b0738abb3f6f119ca2affd3fbaa
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2018-12-26 03:55:30 +0300

    отступы

--- List/main_List.cpp
+++ List/main_List.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	std::cout << "An example of using the List will be implemented here" << std::endl;
 	TList <int> L1;
-	L1.PutBegin(60);
+	L1.PutBegin(70);
 	L1.PutBegin(40);
 	L1.PutBegin(50);
 	std::cout << L1.GetBegin() << "\n";
