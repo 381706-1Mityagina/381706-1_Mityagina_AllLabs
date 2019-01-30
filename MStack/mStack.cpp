8b8d5deb1f0f3954e24e2c8c061db14f258311aa
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2019-01-30 04:10:14 +0300

    нью

--- MStack/mStack.cpp
+++ MStack/mStack.cpp
@@ -1,19 +1,24 @@
 #include "..//MStackLib/MStack.h"
 #include "..//Exception/Exception.h"
+#include <time.h>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	std::cout << "An example of using the MStack will be implemented here" << std::endl;
-	TMStack <int> MS(10, 3);
+	std::cout << "\nAn example of using the MStack will be implemented here" << std::endl;
+	TMStack <int> MS(3, 11);
 //	MS.Print();
 	MS.Set(2, 1);
 	MS.Set(2, 2);
 	MS.Set(2, 3);
 //	MS.Print();
+	float t1 = 0, t2 = 0;
+	t1 = clock();
 	MS.Set(2, 4);
+	t2 = (clock() - t1) / CLOCKS_PER_SEC;
+	std::cout << t2 << "\n";
 	TMStack <int> M2(MS);
 	try 
 	{
@@ -23,9 +28,11 @@ int main()
 		std::cout << MS.Get(2);
 		// std::cout << MS.Get(2);
 	}
-	catch (TException exp) {
+	catch (TException exp) 
+	{
 		exp.Show();
 	}
 	//system("pause");
+	std::cout << "\n";
 	return 0;
 }
