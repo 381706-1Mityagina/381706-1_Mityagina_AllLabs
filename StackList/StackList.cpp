9431859fa696f4d9464933089d9ebc2bfb5bbf12
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2019-02-06 01:10:37 +0300

    ?

--- StackList/StackList.cpp
+++ StackList/StackList.cpp
@@ -7,7 +7,32 @@ int main()
 {
   cout << "An example of using the StackList will be implemented here" << endl;
   
-  /* smth interesting */
-  
-  return 0;
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+	cout << "\t <<< Введите кол-во элементов в стеке >>>\n";
+	do
+		cin >> n; while (n < 0);
+	TStackUsingList<char> symbolStack(n);
+	char symbol;
+
+	cout << "\t <<< Введите элементы стека >>> \n";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> symbol;
+		symbolStack.Put(symbol); // помещаем элементы в стек
+	}
+
+	cout << "\n";
+
+	symbolStack.Show(); // печать стека
+
+	cout << "\n\t <<< Удалим элемент из стека >>>\n";
+	symbolStack.Del();
+
+	symbolStack.Show(); // печать стека
+
+	cout << "\n\n";
+
+	return 0;
 }
