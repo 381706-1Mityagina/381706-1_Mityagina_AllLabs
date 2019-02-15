b74fab0d55ddfaeffe0ca4e8d6ad8f7071450578
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2019-02-15 18:55:43 +0300

    табы

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -6,34 +6,34 @@ using namespace std;
 
 int main()
 {	
-	setlocale(LC_ALL, "Russian");
+  setlocale(LC_ALL, "Russian");
 
-	int n;
-	cout << "Введите кол-во элементов в стеке\n";
-	do 
-		cin >> n; while (n < 0);
-	TStack<char> symbolStack(n);
-	char symbol;
-	cout << "Введите элементы стека\n";
-	for (int i = 0; i < n; i++)
-	{
-		cin >> symbol;
-		symbolStack.Put(symbol); // помещаем элементы в стек
-	}
+  int n;
+  cout << "Введите кол-во элементов в стеке\n";
+  do 
+  cin >> n; while (n < 0);
+  TStack<char> symbolStack(n);
+  char symbol;
+  cout << "Введите элементы стека\n";
+  for (int i = 0; i < n; i++)
+  {
+    cin >> symbol;
+    symbolStack.Put(symbol); // помещаем элементы в стек
+  }
 
-	cout << "\n";
+  cout << "\n";
 
-	symbolStack.PrintStack(); // печать стека
+  symbolStack.PrintStack(); // печать стека
 
-	cout << "\n\nУдалим элемент из стека\n";
-	symbolStack.Get();
+  cout << "\n\nУдалим элемент из стека\n";
+  symbolStack.Get();
 
-	symbolStack.PrintStack(); // печать стека
+  symbolStack.PrintStack(); // печать стека
 
-	TStack<char> newOne(symbolStack);
+  TStack<char> newOne(symbolStack);
 
-	cout << "\n\nКонструктор копирования в действии:\n";
-	newOne.PrintStack();
+  cout << "\n\nКонструктор копирования в действии:\n";
+  newOne.PrintStack();
 
-	return 0;
+  return 0;
 }
