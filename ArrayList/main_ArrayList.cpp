d8a51d5a36cfc3bfe73339073be695332138d393
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2019-02-15 18:40:17 +0300

    табы

--- ArrayList/main_ArrayList.cpp
+++ ArrayList/main_ArrayList.cpp
@@ -3,12 +3,12 @@
 
 int main()
 {
-	std::cout << "An example of using the List will be implemented here" << std::endl;
-	TArrayList <int> L1;
-	L1.PutBegin(60);
-	L1.PutBegin(40);
-	std::cout << L1.GetBegin();
-	std::cout << "\n";
-	//system("pause");
+  std::cout << "An example of using the List will be implemented here" << std::endl;
+  TArrayList <int> L1;
+  L1.PutBegin(60);
+  L1.PutBegin(40);
+  std::cout << L1.GetBegin();
+  std::cout << "\n";
+  //system("pause");
   return 0;
 }
