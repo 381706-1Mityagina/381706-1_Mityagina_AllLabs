634814d44ecc3fb97e3c9411aac70b2a9f521c7d
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2019-03-12 03:36:18 +0300

    Update Queue.h

--- QueueLib/Queue.h
+++ QueueLib/Queue.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "..//StackLib/Stack.h"
+
 template <class T>
 class TQueue : public TStack <T> 
 {
@@ -8,47 +9,28 @@ protected:
   int Start; // Начало очереди
   int Count; // Кол-во элементов в очереди -> =конец очереди
 public:
-  TQueue();//Конструктор по умолчанию
   TQueue(int N = 0); // конструктор с параметром
   TQueue(TQueue <T> &Q); // конструктор копирования
   T Get(); // взять элемент
+  void ShowQ(); // вывести элементы очереди
+  T GetButDontDelQ(); // получить вершину(начало) очереди
   void Put(T A); // положить в конец очереди
   bool IsFull(); // проверка на полноту
   bool IsEmpty(); // проверка на пустоту
 };
 //-----------------------------------------------------------------
 template <class T>
-TQueue<T>::TQueue() : TStack<T>()
-{
-	Start = Count = 0;
-}
-//-----------------------------------------------------------------
-template <class T>
 TQueue<T>::TQueue(int N) : TStack<T>(N) 
 {
-	Start = 0;
-	Count = 0; 
+  Start = 0;
+  Count = 0; 
 }
 //-----------------------------------------------------------------
 template <class T>
 TQueue<T>::TQueue(TQueue<T> &Q) : TStack<T>(Q) 
 { 
-	Start = Q.Start; 
-	Count = Q.Count; 
-}
-//-----------------------------------------------------------------
-template <class T>
-T TQueue<T>::Get() 
-{
-if (IsEmpty())
- throw TException("Queue is empty.");
-else 
-{
- T tmp = TStack<T>::Mas[Start];
- Start = (Start + 1) % TStack<T>::Size;
- Count--;
-return tmp;
- }
+  Start = Q.Start; 
+  Count = Q.Count; 
 }
 //-----------------------------------------------------------------
 template <class T>
@@ -58,10 +40,41 @@ if (IsFull())
  throw TException("Queue is full.");
 else 
 {
- TStack<T>::Mas[TStack<T>::Top] = A;
- TStack<T>::Top = (TStack<T>::Top + 1) % TStack<T>::Size;
- Count++;
+  /*TStack<T>::mas[Start] = A;
+  Start = (Start + 1) % TStack<T>::size;*/
+  TStack<T>::Put(A);
+  TStack<T>::Top = TStack<T>::Top % TStack<T>::Size;
+  Count++;
  }
+}
+//-----------------------------------------------------------------
+template <class T>
+T TQueue<T>::Get() 
+{
+if (IsEmpty())
+ throw TException("Queue is empty.");
+else 
+{
+ T temporary = TStack<T>::Mas[Start];
+ Start = (Start + 1) % TStack<T>::Size;
+ Count--;
+ return temporary;
+ }
+}
+//-----------------------------------------------------------------
+template <class T>
+T TQueue<T>::GetButDontDelQ()
+{
+  return TStack<T>::Mas[Start];
+}
+//-----------------------------------------------------------------
+template <class T>
+void TQueue<T>::ShowQ()
+{
+  int t = TStack<T>::Top;
+  int s = TStack<T>::Size;
+  for (int i = Start; i < t; i = (i + 1) % s)
+  cout << TStack<T>::Mas[i];
 }
 //-----------------------------------------------------------------
 template <class T>
