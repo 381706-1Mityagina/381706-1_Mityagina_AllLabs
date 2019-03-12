a81b4a554920379663da421e1ab38145acb5cf82
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2019-03-12 03:43:49 +0300

    Create Queue_Test.cpp

---
+++ QueueTest/Queue_Test.cpp
@@ -0,0 +1,98 @@
+#include "..//QueueLib/Queue.h"
+
+#include "gtest.h"
+
+TEST(TQueue, can_create_queue_with_positive_size)
+{
+	ASSERT_NO_THROW(TQueue <int> V(7));
+}
+//------------------------------------------------------------------------
+TEST(TQueue, cannot_create_queue_with_negative_size)
+{
+	ASSERT_ANY_THROW(TQueue <int> V(-7));
+}
+//------------------------------------------------------------------------
+TEST(TQueue, can_do_stuff_like_put_and_get)
+{
+	TQueue <int> Q(1);
+	
+	Q.Put(7);
+	
+	ASSERT_EQ(7, Q.Get());
+}
+//------------------------------------------------------------------------
+TEST(TQueue, can_tell_if_Queue_is_not_full)
+{
+	TQueue <int> Q(7);
+	
+	ASSERT_FALSE(Q.IsFull());
+}
+//------------------------------------------------------------------------
+TEST(TQueue, can_copy_queue)
+{
+	TQueue<double> Q(2);
+	Q.Put(5);
+
+  EXPECT_NO_THROW(TQueue<double> Q2(Q));
+}
+//------------------------------------------------------------------------
+TEST(TQueue, can_take_elem_from_not_empty_queue)
+{
+	TQueue<double> Q(2);
+	
+	Q.Put(1);
+	
+	ASSERT_NO_THROW(Q.Get());
+}
+//------------------------------------------------------------------------
+TEST(TQueue, cant_put_elem_in_full_queue)
+{
+	TQueue<double> Q(1);
+	
+	Q.Put(1);
+	
+	ASSERT_ANY_THROW(Q.Put(1));
+}
+//------------------------------------------------------------------------
+TEST(TQueue, can_take_elem_from_queue)
+{
+	TQueue<double> Q(1);
+	
+	Q.Put(5);
+        TQueue<double> Q2(Q);
+  
+	EXPECT_EQ(5, Q2.Get());
+}
+//------------------------------------------------------------------------
+TEST(TQueue, can_check_if_queue_is_empty_false_check)
+{
+	TQueue<double> Q(1);
+	
+	Q.Put(5);
+
+	EXPECT_EQ(0, Q.IsEmpty());
+}
+//------------------------------------------------------------------------
+TEST(TQueue, can_check_if_queue_is_full)
+{
+	TQueue<double> Q(1);
+	
+	Q.Put(5);
+
+	EXPECT_EQ(1, Q.IsFull());
+}
+//------------------------------------------------------------------------
+TEST(TQueue, can_get_in_the_right_order)
+{
+  TQueue<int> queue(4);
+	
+  queue.Put(1);
+  queue.Put(2);
+  queue.Put(3);
+  queue.Put(5);
+	
+  ASSERT_EQ(queue.Get(), 1);
+  ASSERT_EQ(queue.Get(), 2);
+  ASSERT_EQ(queue.Get(), 3);
+  ASSERT_EQ(queue.Get(), 5);
+}
