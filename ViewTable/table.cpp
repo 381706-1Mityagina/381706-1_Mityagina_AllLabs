f1d5d6d8bb74c90257c99d4c7bc490b136f3bca5
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2019-06-29 16:03:22 +0300

    ?

--- ViewTable/table.cpp
+++ ViewTable/table.cpp
@@ -6,19 +6,16 @@
 using namespace std;
 
 int main()
-{
-  TViewTableElem<int> elem_f("a"), elem_s("b"), elem_t("c");
-  TViewTableElem<int> elem_fo(elem_f);
-  
+{  
   TViewTable<int> hey(10);
   
-  hey.Put(elem_s.key, 10);
-  hey.Put(elem_t.key, 100);
-  hey.Put(elem_f.key, 40);
+  hey.Put(b, 10);
+  hey.Put(c, 100);
+  hey.Put(a, 40);
 
   cout<<t<<"\n";
   
-  hey.Put(elem_fo.key, 40);
+  hey.Put(a, 40);
   
   return 0;
 }
