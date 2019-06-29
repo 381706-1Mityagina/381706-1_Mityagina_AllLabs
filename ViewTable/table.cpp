752a103b6c91c93056a700595835fc9579d20505
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2019-06-29 15:54:00 +0300

    main

--- ViewTable/table.cpp
+++ ViewTable/table.cpp
@@ -7,5 +7,18 @@ using namespace std;
 
 int main()
 {
+  TViewTableElem<int> elem_f("a", 40), elem_s("b", 10), elem_t("c", 100);
+  TViewTableElem<int> elem_fo(elem_f);
+  
+  TViewTable<int> hey(10);
+  
+  hey.Put(elem_s);
+  hey.Put(elem_t);
+  hey.Put(elem_f);
+
+  cout<<t<<"\n";
+  
+  hey.Put(elem_fo);
+  
   return 0;
 }
